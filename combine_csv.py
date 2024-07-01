967f9165ea4d27d6072309a1043266f014bfd41e
Ann Song <gmsong1978@gmail.com>
2024-07-01 12:04:15 -0400

    Refactored

--- combine_csv.py
+++ combine_csv.py
@@ -1,6 +1,16 @@
 import os
 import pandas as pd
 
+def combine_data(dataframes):
+    if dataframes:
+        combined_df = pd.concat(dataframes, ignore_index=True).drop_duplicates(subset='pdb_file')
+        combined_df.to_csv('full_data.csv', index=False)
+        print("All CSV files combined and saved as 'full_data.csv'.")
+    else:
+        print("No CSV files found in the directory.")
+
+
+
 def main(dir: str):
     dataframes = []
 
@@ -12,12 +22,7 @@ def main(dir: str):
             df = pd.read_csv(file_path)
             dataframes.append(df)
 
-    if dataframes:
-        combined_df = pd.concat(dataframes, ignore_index=True).drop_duplicates(subset='pdb_file')
-        combined_df.to_csv('full_data.csv', index=False)
-        print("All CSV files combined and saved as 'full_data.csv'.")
-    else:
-        print("No CSV files found in the directory.")
+    
 
 if __name__ == "__main__":
     main("all_csvs")
