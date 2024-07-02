1341713f4c28fb893fd22d0ea7ee8c1d797806f9
Ann Song <gmsong1978@gmail.com>
2024-07-02 11:05:22 -0400

    Combine csv working

--- reformat_data/combine_csv.py
+++ reformat_data/combine_csv.py
@@ -1,28 +1,22 @@
-import os
 import pandas as pd
-
-def combine_data(dataframes):
-    if dataframes:
-        combined_df = pd.concat(dataframes, ignore_index=True).drop_duplicates(subset='pdb_file')
-        combined_df.to_csv('full_data.csv', index=False)
-        print("All CSV files combined and saved as 'full_data.csv'.")
-    else:
-        print("No CSV files found in the directory.")
+import os
 
 
+def main(full_dir: str):
+    csv_files = []
+    for data_category in os.listdir(full_dir):
+        for data_csv in os.listdir(os.path.join(full_dir, data_category)):
+            csv_files.append(os.path.join(os.path.join(full_dir, data_category), data_csv))
+            print(data_csv)
+    combined_df = pd.DataFrame()
 
-def main(dir: str):
-    dataframes = []
+    for file in csv_files:
+        df = pd.read_csv(file)
+        combined_df = pd.concat([combined_df, df], ignore_index=True)
 
-    for file in os.listdir(dir):
-        file_path = os.path.join(dir, file)
+    # Save the combined DataFrame to a new CSV file
+    combined_df.to_csv('combined_interface_rsm.csv', index=False)
 
-        if os.path.isfile(file_path) and file.endswith('.csv'):
-            print(f"Processing CSV file: {file}")
-            df = pd.read_csv(file_path)
-            dataframes.append(df)
-
-    
-
-if __name__ == "__main__":
-    main("../all_csvs")
+if __name__=="__main__":
+    main('all_csvs')
+    print("CSV files have been successfully combined into 'combined_interface_rsm.csv'")
