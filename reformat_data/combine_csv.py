eb6c1950ac18d55c8fd5bdff4a42ddf62ed428d9
Ann Song <gmsong1978@gmail.com>
2024-07-02 11:06:23 -0400

    Renamed combined data

--- reformat_data/combine_csv.py
+++ reformat_data/combine_csv.py
@@ -7,7 +7,6 @@ def main(full_dir: str):
     for data_category in os.listdir(full_dir):
         for data_csv in os.listdir(os.path.join(full_dir, data_category)):
             csv_files.append(os.path.join(os.path.join(full_dir, data_category), data_csv))
-            print(data_csv)
     combined_df = pd.DataFrame()
 
     for file in csv_files:
@@ -15,7 +14,7 @@ def main(full_dir: str):
         combined_df = pd.concat([combined_df, df], ignore_index=True)
 
     # Save the combined DataFrame to a new CSV file
-    combined_df.to_csv('combined_interface_rsm.csv', index=False)
+    combined_df.to_csv('full_data.csv', index=False)
 
 if __name__=="__main__":
     main('all_csvs')
