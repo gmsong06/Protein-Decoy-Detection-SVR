4b42c10919d2ec4d04ac0f99c233026fb2aabb4f
Ann Song <gmsong1978@gmail.com>
2024-07-03 14:18:44 -0400

    Some bug fixes to dockq

--- reformat_data/combine_dockq.py
+++ reformat_data/combine_dockq.py
@@ -5,20 +5,23 @@ import os
 
 main_file = pd.read_csv("combined_data.csv", on_bad_lines='skip')
 
-def dockq(folder_path):
+def dockQ(folder_path):
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
-        if filename.endswith('.csv') and os.path.isdir(file_path):
+        if filename.endswith('.csv'):
             data = pd.read_csv(file_path, on_bad_lines='skip')
             pdb_id = filename[:4]
 
-            for index, value in data['Decoys'].items():
-                        if data.at[index, 'Decoys'][-4:] != pdb_id:
-                            data.at[index, 'Decoys'] = data.at[index, 'Decoys'] + "_" + pdb_id
+            for index, value in data['Decoy'].items():
+                        if data.at[index, 'Decoy'][-4:] != pdb_id:
+                            data.at[index, 'Decoy'] = data.at[index, 'Decoy'] + "_" + pdb_id
                             print("Appending " + pdb_id)
 
             data.to_csv(file_path, index=False)
             pd.concat([main_file, data["DockQ"]],
                   axis = 1)
+            main_file.to_csv('combined_data_2.csv', index=False)
                         
-    
+
+if __name__ == "__main__":
+      dockQ('final_manuscript_supersample_scores')
