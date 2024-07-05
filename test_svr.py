c530856fd064816ca80464f9136c28e6d9a81dd2
Ann Song <gmsong1978@gmail.com>
2024-07-05 11:54:14 -0400

    Testing smth again

--- test_svr.py
+++ test_svr.py
@@ -7,12 +7,41 @@ from sklearn.decomposition import PCA
 import numpy as np
 import os
 import pandas as pd
+from matplotlib import pyplot as plt
+
 
 df = pd.read_csv('combined_data.csv')
 
 train, test = train_test_split(df, test_size=0.2, random_state=42)
 
-train = train.sort_values('all_contacts')
-test = test.sort_values('all_contacts')
+train = train.sort_values('interface_rsm')
+test = test.sort_values('interface_rsm')
 
-print(train.head())
+X_train, X_test = train[['interface_rsm']], test[['interface_rsm']]
+y_train, y_test = train['all_contacts'], test['all_contacts']
+
+scaler = StandardScaler().fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+svr_lin = SVR(kernel = 'linear')
+svr_rbf = SVR(kernel = 'rbf')
+svr_poly = SVR(kernel = 'poly')
+
+svr_lin.fit(X_train_scaled, y_train)
+svr_rbf.fit(X_train_scaled, y_train)
+svr_poly.fit(X_train_scaled, y_train)
+
+#### Model prediction for train dataset ####
+train['linear_svr_pred'] = svr_lin.predict(X_train_scaled)
+train['rbf_svr_pred'] = svr_rbf.predict(X_train_scaled)
+train['poly_svr_pred'] = svr_poly.predict(X_train_scaled)
+
+#### Visualization ####
+plt.scatter(train['time_study'], train['Marks'])
+plt.plot(train['time_study'], train['linear_svr_pred'], color = 'orange', label = 'linear SVR')
+plt.plot(train['time_study'], train['rbf_svr_pred'], color = 'green', label = 'rbf SVR')
+plt.plot(train['time_study'], train['poly_svr_pred'], color = 'blue', label = 'poly SVR')
+plt.legend()
+plt.xlabel('Study time')
+plt.ylabel('Marks')
