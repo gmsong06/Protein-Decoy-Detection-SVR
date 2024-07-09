05812c6308b239b14c553b0bdb8757edb8e20cea
Ann Song <gmsong1978@gmail.com>
2024-07-09 11:07:25 -0400

    Spearman script done

--- find_spearman/find_spearman.py
+++ find_spearman/find_spearman.py
@@ -7,7 +7,14 @@ args = parser.parse_args()
 
 def main():
     df = pd.read_csv(args.predictions_path)
+    df['pdb_id'] = df['pdb_file'].str[-4:]
 
+    pdb_dict = {pdb_id: df[df['pdb_id'] == pdb_id] for pdb_id in df['pdb_id'].unique()}
+    
+    print("Unique pdb_ids:", pdb_dict.keys())
 
-if __name__=="__main__":
+    for pdb_id, pdb_df in pdb_dict.items():
+        pdb_df.to_csv(f"{pdb_id}.csv", index=False)
+
+if __name__ == "__main__":
     main()
