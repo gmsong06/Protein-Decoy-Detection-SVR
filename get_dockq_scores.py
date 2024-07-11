a01049a57c02c822c10839afd85a6713197005ec
Ann Song <gmsong1978@gmail.com>
2024-07-11 11:05:42 -0400

    Added modified chain id function

--- get_dockq_scores.py
+++ get_dockq_scores.py
@@ -1,3 +1,6 @@
+import Bio.PDB.PDBIO
+import Bio.PDB.PDBParser
+import Bio.PDB.PDBParser
 import pandas as pd
 from DockQ.DockQ import load_PDB, run_on_all_native_interfaces
 import Bio.PDB
@@ -27,7 +30,39 @@ def get_chain_ids(structure):
                 for atom in chain:
                     atoms +=1 
     return [chain_id_1, chain_id_2]
-    
+
+def modify_chain_ids(pdb_filename, current_chain_ids, desired_chain_ids, output_filename):
+    """
+    Modify the chain IDs in a PDB file to the desired chain IDs.
+
+    Args:
+    pdb_filename (str): The input PDB file path.
+    current_chain_ids (list): The current array of two chain IDs.
+    desired_chain_ids (list): The desired array of two chain IDs.
+    output_filename (str): The output PDB file path with modified chain IDs.
+
+    Returns:
+    None
+    """
+    # Ensure that the current and desired chain IDs have the same length
+    if len(current_chain_ids) != 2 or len(desired_chain_ids) != 2:
+        raise ValueError("Both current_chain_ids and desired_chain_ids must be lists of two elements.")
+
+    parser = Bio.PDB.PDBParser()
+    structure = parser.get_structure('structure', pdb_filename)
+
+    # Modify chain IDs
+    for model in structure:
+        for chain in model:
+            if chain.id == current_chain_ids[0]:
+                chain.id = desired_chain_ids[0]
+            elif chain.id == current_chain_ids[1]:
+                chain.id = desired_chain_ids[1]
+
+    # Save the modified structure to a new file
+    io = Bio.PDB.PDBIO()
+    io.set_structure(structure)
+    io.save(output_filename)
 
 
 def main():
@@ -46,14 +81,14 @@ def main():
                 type = "relaxed"
 
             if type == "relaxed":
-                model_path = f"/home/ms4688/palmer_scratch/Capri_SuperSampled/sampled_{pdb_id}/sampled_{pdb_id}_relaxed/{file_name}.pdb"
+                model_path = f"/home/as4643/palmer_scratch/Capri_SuperSampled/sampled_{pdb_id}/sampled_{pdb_id}_relaxed/{file_name}.pdb"
             else:
-                model_path = f"/home/ms4688/palmer_scratch/Capri_SuperSampled/sampled_{pdb_id}/random_negatives/random_{pdb_id}_relaxed/{file_name}.pdb"
+                model_path = f"/home/as4643/palmer_scratch/Capri_SuperSampled/sampled_{pdb_id}/random_negatives/random_{pdb_id}_relaxed/{file_name}.pdb"
         elif file_name.startswith("random"):
             print(file_name)
-            model_path = f"/home/ms4688/palmer_scratch/Capri_SuperSampled/sampled_{pdb_id}/random_negatives/random_{pdb_id}_relaxed/{file_name}.pdb"
+            model_path = f"/home/as4643/palmer_scratch/Capri_SuperSampled/sampled_{pdb_id}/random_negatives/random_{pdb_id}_relaxed/{file_name}.pdb"
         
-        native = load_PDB(f"/home/ms4688/palmer_scratch/Protein-Decoy-Detection-SVR/capri_targets/Target{pdb_id[-2:]}.pdb")
+        native = load_PDB(f"/home/as4643/palmer_scratch/Capri_SuperSampled/targets/Target{pdb_id[-2:]}.pdb")
         print(pdb_id[-2:])
         model = load_PDB(model_path)
 
