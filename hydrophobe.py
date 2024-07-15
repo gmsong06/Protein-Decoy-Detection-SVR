f6dc2b8ac522596e2771e0a5595e71ee314a214f
Smriti Rajan <smritirajan211@gmail.com>
2024-07-15 15:17:58 -0400

    Reset file

--- hydrophobe.py
+++ hydrophobe.py
@@ -1,9 +1,13 @@
 from pdb_reader import Protein
+import math
 import numpy as np
+import pandas as pd
+import Bio
+from Bio.PDB import PDBParser,vectors
+from pathlib import Path
 import os
 import argparse
 import csv
-from concurrent.futures import ProcessPoolExecutor, as_completed
 
 parser = argparse.ArgumentParser()
 parser.add_argument("pdb_folder", type=str, help="Path to the folder containing PDB files")
@@ -18,11 +22,10 @@ def get_residue_name(prot, residue_id):
     return None
 
 def get_residues(prot):
-    atom_names = []
+    coordinates = []
+    atom_names = []                
     chains = []
     residue_ids = []
-    coordinates = []
-
     for model in prot.structure:
         for chain in model:
             for residue in chain:
@@ -32,67 +35,80 @@ def get_residues(prot):
                     coordinates.append(atom.coord)
                     residue_ids.append(residue.id[1])
 
-    atom_names = np.array(atom_names)
-    coordinates = np.array(coordinates)
-    chains = np.array(chains)
-    residue_ids = np.array(residue_ids)
+    def hydrogen_list():
+        for i in range(len(atom_names)):
+            first_letter = atom_names[i][0]
+            if first_letter == "H":
+                atom_names[i] = "hydrogen"
+            elif first_letter.isnumeric():
+                if atom_names[i][1] == "H":
+                    atom_names[i] = "hydrogen"
+                else:
+                    atom_names[i] == "not_hydrogen"
+            else:
+                atom_names[i] = "not_hydrogen"
 
-    mask = np.char.startswith(atom_names, 'H') | np.char.startswith(atom_names, '1H') | np.char.startswith(atom_names, '2H') | np.char.startswith(atom_names, '3H')
-    atom_names[mask] = 'hydrogen'
-    atom_names[~mask] = 'not_hydrogen'
+    hydrogen_list()
 
-    listA, listB, residue_ids_A, residue_ids_B = [], [], [], []
+    listA = []
+    listB = []
+    residue_ids_A = []
+    residue_ids_B = []
     lst = prot.get_chain_ids()
-    maskA = (chains == lst[0]) & (atom_names == 'not_hydrogen')
-    maskB = (chains == lst[1]) & (atom_names == 'not_hydrogen')
-
-    listA = coordinates[maskA]
-    residue_ids_A = residue_ids[maskA]
-    listB = coordinates[maskB]
-    residue_ids_B = residue_ids[maskB]
+    for i in range(len(coordinates)):
+        if ((chains[i] == lst[0]) and (atom_names[i] == "not_hydrogen")):
+            listA.append(coordinates[i])
+            residue_ids_A.append(residue_ids[i])
+        elif ((chains[i] == lst[1]) and (atom_names[i] == "not_hydrogen")):
+            listB.append(coordinates[i])
+            residue_ids_B.append(residue_ids[i])      
 
     dist_thresh = 5
     res_in_contact = []
-
-    for i, posA in enumerate(listA):
-        distances = np.linalg.norm(listB - posA, axis=1)
-        indices = np.where(distances <= dist_thresh)[0]
-        for idx in indices:
-            res_in_contact.append([residue_ids_A[i], residue_ids_B[idx]])
+    for i,posA in enumerate(listA):
+        for j,posB in enumerate(listB):
+            if np.linalg.norm(np.array(posA) - np.array(posB)) <= dist_thresh:
+                cont = [residue_ids_A[i], residue_ids_B[j]]
+                if cont not in res_in_contact:
+                    res_in_contact.append(cont)
 
     return res_in_contact
 
 
 def get_hydro_hits(file):
+
     hydrophobicity_dict = {
-        "ARG": 0.72002943, "ASP": 0.75367063, "GLU": 0.87591947, "LYS": 1, "ASN": 0.67819213,
-        "GLN": 0.72278272, "PRO": 0.65123555, "HIS":  0.48907553, "SER": 0.52365422, "THR": 0.47798833,
-        "GLY": 0.46477639, "TYR": 0.21646225, "ALA": 0.30953653, "CYS": 0, "MET": 0.18184843,
-        "TRP":  0.14290738, "VAL": 0.10992156, "PHE": 0.0814021, "LEU": 0.10211201, "ILE": 0.06280283
-    }
+            "ARG": 0.72002943, "ASP": 0.75367063, "GLU": 0.87591947, "LYS": 1, "ASN": 0.67819213,
+            "GLN": 0.72278272, "PRO": 0.65123555, "HIS":  0.48907553, "SER": 0.52365422, "THR": 0.47798833,
+            "GLY": 0.46477639, "TYR": 0.21646225, "ALA": 0.30953653, "CYS": 0, "MET": 0.18184843,
+            "TRP":  0.14290738, "VAL": 0.10992156, "PHE": 0.0814021, "LEU": 0.10211201, "ILE": 0.06280283
+        }
 
     hits = 0
 
     protein = Protein(file)
     res_list = get_residues(protein)
-    resA = [res[0] for res in res_list]
-    resB = [res[1] for res in res_list]
+    resA = []
+    resB = []
+    print(len(res_list))
+    for res in res_list:
+        resA.append(res[0])
+        resB.append(res[1])
 
+    print(f"Contacts in Chain A: {len(resA)} + Contacts in Chain B: {len(resB)}")
     if len(resA) == len(resB):
-        for i in range(len(resA)):
-            resnameA = get_residue_name(protein, resA[i])
-            resnameB = get_residue_name(protein, resB[i])
-            if abs(hydrophobicity_dict[resnameA] - hydrophobicity_dict[resnameB]) <= 0.3:
-                hits += 1
+        print("Lengths are equal. Proceeding to compare hydrophobicities.")
+    print("-----------------------------------------------------")
+
+    for i in range(len(resA)):
+        resnameA = get_residue_name(protein, resA[i])
+        resnameB = get_residue_name(protein, resB[i])
+        print(f"Contact between {resnameA} and {resnameB}")
+        print(f"Hydrophobicities = {hydrophobicity_dict[resnameA]} and {hydrophobicity_dict[resnameB]}")
+        if abs(hydrophobicity_dict[resnameA] - hydrophobicity_dict[resnameB]) <= 0.3:
+            hits += 1
 
     return hits
-
-
-def process_pdb_file(pdb_path):
-    filename = os.path.basename(pdb_path)
-    result = (filename[:-4], get_hydro_hits(pdb_path))
-    print(result)
-    return result
 
 
 def process_pdb_folder(full_folder_path, pdb_id):
@@ -100,36 +116,33 @@ def process_pdb_folder(full_folder_path, pdb_id):
     relaxed_folder_path = os.path.join(full_folder_path, f"{pdb_id}_relaxed")
     random_folder_path = os.path.join(full_folder_path, f"random_negatives/rand_{pdb_id}_relaxed")
 
-    pdb_files = []
-
-    for path in [relaxed_folder_path, random_folder_path]:
+    paths = [relaxed_folder_path, random_folder_path]
+    for path in paths:
+        print(f"Path is {path}")
         for filename in os.listdir(path):
+            print(f"Filename is {filename}")
             if filename.endswith('.pdb') and ("NoH" not in filename):
-                pdb_files.append(os.path.join(path, filename))
-
-    with ProcessPoolExecutor() as executor:
-        future_to_pdb = {executor.submit(process_pdb_file, pdb_file): pdb_file for pdb_file in pdb_files}
-        for future in as_completed(future_to_pdb):
-            try:
-                result = future.result()
-                results.append(result)
-            except Exception as exc:
-                print(f'Generated an exception: {exc}')
-
-    output_csv = f'{pdb_id}_hydrophobicity_contacts.csv'
+                pdb_path = os.path.join(path, filename)
+                print(f"Processing {filename}")
+                results.append((filename[:-4], (get_hydro_hits(pdb_path))))
+            else:
+                print(f"File did not pass requirements.")
+    output_csv = f'{pdb_id}hydrophobicity_contacts.csv'
     with open(output_csv, mode='w', newline='') as file:
+
         writer = csv.writer(file)
         writer.writerow(['pdb_file', 'hydrophobicity_contacts'])
         for result in results:
             writer.writerow(result)
-
 
 def main(folder_path):
     for folder in os.listdir(folder_path):
         full_folder_path = os.path.join(folder_path, folder)
         if folder.startswith("sampled_") and os.path.isdir(full_folder_path):
             pdb_id = full_folder_path[-4:]
+            print(f"PDB id is {pdb_id}")
             process_pdb_folder(full_folder_path, pdb_id)
+            print("DONE----------------------------------------------------------------------")
 
 if __name__ == "__main__":
     main(args.pdb_folder)
