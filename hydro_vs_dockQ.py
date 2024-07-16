86b0004f4298c66a4cfd4a8616319015d0cb9369
Smriti Rajan <smritirajan211@gmail.com>
2024-07-16 10:06:39 -0400

    Changed spearman script to pearson

--- hydro_vs_dockQ.py
+++ hydro_vs_dockQ.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import argparse
 import matplotlib.pyplot as plt
-from scipy.stats import spearmanr
+from scipy.stats import pearsonr
 
 parser = argparse.ArgumentParser()
 parser.add_argument("pdb_folder", type=str, help="Path to the folder containing PDB files")
@@ -26,19 +26,19 @@ def main(folder_path):
         for index, row in df.iterrows():
             hydro_scores.append(row['hydrophobicity_contacts'])
         
-        spearman_corr, spearman_p = spearmanr(dockq_scores, hydro_scores)
+        pearson_corr, pearson_p = pearsonr(dockq_scores, hydro_scores)
         
         # Append results to the list
-        results.append({'pdb_id': pdb_id, 'spearman_correlation': spearman_corr})
+        results.append({'pdb_id': pdb_id, 'pearson_correlation': pearson_corr})
 
         fig, ax = plt.subplots(dpi=150, figsize=(5, 5))
         ax.scatter(hydro_scores, dockq_scores, edgecolor='black', color='none')
 
         ax.set_xlabel('Contacts of similar Hydrophbicity', fontsize=12)
         ax.set_ylabel('DockQ', fontsize=12)
-        ax.set_title(f'Spearman Correlation: {spearman_corr:.2f}', fontsize=12)
+        ax.set_title(f'Pearson Correlation: {pearson_corr:.2f}', fontsize=12)
 
-        plot_filename = os.path.join("/vast/palmer/scratch/ohern/sr2562/Protein-Decoy-Detection-SVR/spearman_plots/hydro", f"{pdb_id}.png")
+        plot_filename = os.path.join("/vast/palmer/scratch/ohern/sr2562/Protein-Decoy-Detection-SVR/pearson_plots/hydro", f"{pdb_id}.png")
         plt.savefig(plot_filename)
         plt.close(fig)  # Close the figure to avoid memory issues
     
