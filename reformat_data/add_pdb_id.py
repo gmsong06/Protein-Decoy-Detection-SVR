9bf5beaefe13cda20afb51dfdd3c1810e1ec4f33
Ann Song <gmsong1978@gmail.com>
2024-07-22 09:53:52 -0400

    Merged data, T30 missing

--- reformat_data/add_pdb_id.py
+++ reformat_data/add_pdb_id.py
@@ -4,13 +4,13 @@ import os
 def main():
     df = pd.read_csv('combined_data.csv')
 
-    df['pdb_id'] = df['pdb_file'].str[-4:]
+    df['pdb_id'] = df['pdb_file'].str[-3:]
 
     # for index, row in df.iterrows():
     #     row['pdb_id'] = row['pdb_file'][-4:]
     
     # print(df['pdb_id'])
-    df.to_csv('final_data_hydro_groups.csv', index=None)
+    df.to_csv('final_data.csv', index=None)
 
 
 if __name__=="__main__":
