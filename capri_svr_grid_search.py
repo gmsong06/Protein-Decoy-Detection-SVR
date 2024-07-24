ba0f04e0ff4d7662be4ca1520e55a16573da0673
Ann Song <gmsong1978@gmail.com>
2024-07-24 09:35:37 -0400

    Grid search for gamma

--- capri_svr_grid_search.py
+++ capri_svr_grid_search.py
@@ -12,8 +12,20 @@ parser.add_argument("--output_path", type=str, help="Path to output prediction f
 args = parser.parse_args()
 
 # Load the data
-train = pd.read_csv('/home/as4643/palmer_scratch/Protein-Decoy-Detection-SVR/final_data_groups.csv')
-test = pd.read_csv('/home/as4643/palmer_scratch/Protein-Decoy-Detection-SVR/final_data_capri_groups.csv')
+train = pd.read_csv('final_data_groups_hydro.csv')
+test = pd.read_csv('final_data_capri_with_hydro.csv')
+
+specific_column = 'DockQ'
+missing_in_specific_column = test[specific_column].isnull()
+
+# Extracting the 'pdb_file' values where 'DockQ' is missing
+missing_pdb_files = test[missing_in_specific_column]['pdb_file']
+
+# Printing the missing 'pdb_file' values
+print(missing_pdb_files)
+
+# Saving the missing 'pdb_file' values to a text file
+missing_pdb_files.to_csv('missing_pdb_files.txt', index=False, header=False)
 
 to_remove = args.remove
 
@@ -39,39 +51,44 @@ predictions = []
 
 pipeline = Pipeline([
     ('scaler', StandardScaler()),
-    ('svr', SVR())
+    ('svr', SVR(kernel='poly'))
 ])
 
-param_grid = {
-    'svr__kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
-    'svr__C': [0.1, 1, 10, 100],
-    'svr__epsilon': [0.1, 0.2, 0.5, 0.3],
-    'svr__degree': [2, 3, 4], 
-    'svr__gamma': ['scale', 'auto'] 
-}
+# Define parameter grid for gamma
+param_grid = {'svr__gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
 
-grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='r2', n_jobs=-1, verbose=2)
-
+# Perform GridSearchCV to find the best gamma
+grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='neg_mean_absolute_error')
 print("BEGINNING GRID SEARCH")
 grid_search.fit(X_train, y_train)
 print("DONE GRID SEARCH")
 
-print("Best parameters found: ", grid_search.best_params_)
-print("Best score achieved: ", grid_search.best_score_)
-
-# Using the best estimator to make predictions
 best_pipeline = grid_search.best_estimator_
+best_gamma = grid_search.best_params_['svr__gamma']
+print(f"Best gamma: {best_gamma}")
+
+# Evaluate on training set
+y_train_pred = best_pipeline.predict(X_train)
+mae_train = mean_absolute_error(y_train, y_train_pred)
+mse_train = mean_squared_error(y_train, y_train_pred)
+r2_train = r2_score(y_train, y_train_pred)
+
+# Evaluate on test set
 y_pred = best_pipeline.predict(X_test)
 print("DONE PREDICTING")
 
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+mae_test = mean_absolute_error(y_test, y_pred)
+mse_test = mean_squared_error(y_test, y_pred)
+r2_test = r2_score(y_test, y_pred)
 
 results.append({
-    'MAE': mae,
-    'MSE': mse,
-    'R2': r2
+    'MAE_Train': mae_train,
+    'MSE_Train': mse_train,
+    'R2_Train': r2_train,
+    'MAE_Test': mae_test,
+    'MSE_Test': mse_test,
+    'R2_Test': r2_test,
+    'Best_Gamma': best_gamma
 })
 
 for pdb, actual, pred in zip(pdb_files, y_test, y_pred):
