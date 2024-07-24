2431c250466ccea3bee09cf2d0a1e78bac75dcbe
Smriti Rajan <smritirajan211@gmail.com>
2024-07-24 15:41:46 -0400

    Hydro changes

--- hydrophobicity/hydrophobe.py
+++ hydrophobicity/hydrophobe.py
@@ -3,73 +3,59 @@ import os
 import argparse
 import csv
 import h5py
+import math
 
 parser = argparse.ArgumentParser()
 parser.add_argument("pdb_folder", type=str, help="Path to the folder containing PDB files")
 args = parser.parse_args()
 
-def process_hdf5_file(pdb_id, model_id, hdf5_filepath):
-    results = []
-
+def process_hdf5_file(hdf5_filepath):
     hydrophobicity_dict = {
             "ARG": 0.72002943, "ASP": 0.75367063, "GLU": 0.87591947, "LYS": 1, "ASN": 0.67819213,
             "GLN": 0.72278272, "PRO": 0.65123555, "HIS":  0.48907553, "SER": 0.52365422, "THR": 0.47798833,
             "GLY": 0.46477639, "TYR": 0.21646225, "ALA": 0.30953653, "CYS": 0, "MET": 0.18184843,
             "TRP":  0.14290738, "VAL": 0.10992156, "PHE": 0.0814021, "LEU": 0.10211201, "ILE": 0.06280283
         }
-
+    
     with h5py.File(hdf5_filepath, 'r') as hdf5_file:
-        pdb_group = hdf5_file[pdb_id]
-        model_group = pdb_group[model_id]
-        res_contacts = model_group['res_contacts'][:]
-        print(f"Processing Model: {model_id}")
+        pdb_ids = list(f.keys())
+        for pdb_id in pdb_ids:
+            results = []
+            models = list(f[pdb_id].keys())
+            for model in models:
+                res_contacts = f[f"{pdb_id}/{model}/res_contacts"][:]
+                lst = []
 
-        for contact in res_contacts:
-            resnameA, resnameB = contact
-            # print(f"Contact between {resnameA} and {resnameB}")
-            # print(f"Hydrophobicities = {hydrophobicity_dict[resnameA]} and {hydrophobicity_dict[resnameB]}")
-            fx = abs((hydrophobicity_dict[resnameA] - hydrophobicity_dict[resnameB]))
-            results.append(fx)
+                for contact in res_contacts:
+                    resnameA, resnameB = contact
+                    # print(f"Contact between {resnameA} and {resnameB}")
+                    # print(f"Hydrophobicities = {hydrophobicity_dict[resnameA]} and {hydrophobicity_dict[resnameB]}")
+                    fx = abs((hydrophobicity_dict[resnameA] - hydrophobicity_dict[resnameB]))
+                    lst.append(fx)
 
-    return np.mean(results) if results else None
+                fx_mean = np.mean(lst) if lst else None
+                my_sum = 0
+                for item in lst:
+                    my_sum += (item - fx_mean)**2
+            
+                results.append(model[:-4], fx_mean, len(lst), math.sqrt((my_sum/(len(lst)-1))))
+
+        output_csv = f'/vast/palmer/scratch/ohern/sr2562/hydro_results/SD/{pdb_id}_hydrophobicity_fnc.csv'
+        with open(output_csv, mode='w', newline='') as file:
+
+            writer = csv.writer(file)
+            writer.writerow(['pdb_file', 'avg_dif', 'N', 'standard_deviation'])
+            for result in results:
+                writer.writerow(result)
 
 
-def process_pdb_folder(full_folder_path, pdb_id):
-    results = []
-    hdf5_filepath = f'/vast/palmer/scratch/ohern/sr2562/hydro_results/D/{pdb_id}_hydrophobicity_contacts.hdf5'
-
-    relaxed_folder_path = os.path.join(full_folder_path, f"{pdb_id}_relaxed")
-    random_folder_path = os.path.join(full_folder_path, f"random_negatives/rand_{pdb_id}_relaxed")
-
-    paths = [relaxed_folder_path, random_folder_path]
-    for path in paths:
-        print(f"Path is {path}")
-        for filename in os.listdir(path):
-            print(f"Filename is {filename}")
-            if filename.endswith('.pdb') and ("NoH" not in filename):
-                model_id = filename[:-4]
-                print(f"Processing {filename}")
-                avg_fx = process_hdf5_file(pdb_id, model_id, hdf5_filepath)
-                results.append((model_id, avg_fx))
-            else:
-                print(f"File did not pass requirements.")
-
-    output_csv = f'/vast/palmer/scratch/ohern/sr2562/hydro_results/{fnc}/{pdb_id}_hydrophobicity_fnc.csv'
-    with open(output_csv, mode='w', newline='') as file:
-
-        writer = csv.writer(file)
-        writer.writerow(['pdb_file', 'hydrophobicity_fnc'])
-        for result in results:
-            writer.writerow(result)
 
 def main(folder_path):
-    for folder in os.listdir(folder_path):
-        full_folder_path = os.path.join(folder_path, folder)
-        if folder.startswith("sampled_") and os.path.isdir(full_folder_path):
-            pdb_id = full_folder_path[-4:]
-            print(f"PDB id is {pdb_id}")
-            process_pdb_folder(full_folder_path, pdb_id)
-            print("DONE----------------------------------------------------------------------")
+    for file in os.listdir(folder_path):
+        hdf5_filepath = os.path.join(folder_path, file)
+        print(f"Processing {file}")
+        process_hdf5_file(hdf5_filepath)
+        print("DONE----------------------------------------------------------------------")
 
 if __name__ == "__main__":
     main(args.pdb_folder)
--- hydrophobicity/spearman_hydro.py
+++ hydrophobicity/spearman_hydro.py
@@ -5,13 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 #from scipy.stats import pearsonr, spearmanr
 
-hydro = pd.read_csv('hydro_spearman.csv')
-spearman_dict = pd.read_csv("naomi_spearman_dict.csv")
+hydro = pd.read_csv('/Users/smriti/Desktop/aeop/Protein-Decoy-Detection-SVR/hydrophobicity/hydro_spearman.csv')
+contacts = pd.read_csv('/Users/smriti/Desktop/aeop/Protein-Decoy-Detection-SVR/hydrophobicity/contacts_spearman.csv')
+spearman_dict = pd.read_csv("/Users/smriti/Desktop/aeop/Protein-Decoy-Detection-SVR/naomi_spearman_dict.csv")
 
-merge = pd.merge(spearman_dict, hydro, on='pdb', how='outer')
+merge = pd.merge(spearman_dict, (pd.merge(hydro, contacts)), on='pdb', how='outer')
 
 pdb = [] #PDB
 hydro_spearman = [] #HYDRO
+cont_spearman = [] #CONTACTS
 avg_spearman = [] #AVG SPEARMAN
 
 
@@ -23,16 +25,17 @@ for i in range(84):
     pdb.append(i)
 for index, row in result.iterrows():
     hydro_spearman.append(float(row['spearman_correlation'])*(-1))
+    cont_spearman.append(float(row['spearman_correlation_contacts'])*(-1))
     avg_spearman.append(row['avg_spearman']) 
 
-fig, ax = plt.subplots(dpi=150, figsize=(40,2))
+fig, ax = plt.subplots(dpi=150, figsize=(70,2))
 ax.scatter(pdb, hydro_spearman, s=10, label='Hydrophbicity', color='red')
+ax.scatter(pdb, cont_spearman, s=10, label='Contacts', marker = "s", color='blue')
 ax.plot(pdb, avg_spearman, linewidth=1.5, label='Average', color='black')
 # ax.plot(pdb,hydro_spearman,color='red')
-# ax.plot(pdb,avg_spearman,color='black')
 ax.legend(fontsize = 7)
 ax.grid(axis = 'y')
-ax.set_xlabel('PDB', fontsize=7)
+ax.set_xlabel('PDB', fontsize=5)
 ax.set_xticks(pdb)
 ax.tick_params(axis='both', which='major', labelsize=5)
 ax.set_ylabel('Spearman', fontsize=7)
